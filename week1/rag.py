f0c42e5418238501ccdc1b66162f28f84a784053
dingzigeng <1605383434@qq.com>
2026-04-07 15:17:46 +0800

    feat: rag

--- week1/rag.py
+++ week1/rag.py
@@ -37,7 +37,7 @@ QUESTION = (
 
 
 # TODO: Fill this in!
-YOUR_SYSTEM_PROMPT = ""
+YOUR_SYSTEM_PROMPT = "you are a professional Python programmer who writes code to call APIs based on provided documentation. You always follow the requirements and output format specified in the user prompt."
 
 
 # For this simple example
@@ -56,7 +56,7 @@ def YOUR_CONTEXT_PROVIDER(corpus: List[str]) -> List[str]:
 
     For example, return [] to simulate missing context, or [corpus[0]] to include the API docs.
     """
-    return []
+    return [corpus[0]]
 
 
 def make_user_prompt(question: str, context_docs: List[str]) -> str:
